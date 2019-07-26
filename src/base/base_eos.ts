939e2b142754a794173ac297bd0d02f70c6fd015
joy <joy.lai@zg.com>
2019-07-26 12:30:40 +0800

    add

--- src/base/base_eos.ts
+++ src/base/base_eos.ts
@@ -6,11 +6,17 @@
  * EOS系基类
  */
 class BaseEosLike {
-  encodeAmount (amount, symbol, decimals) {
+  /**
+   * 编码amount
+   * @param amount 最小单位
+   * @param symbol 货币名
+   * @param decimals 精度
+   */
+  encodeAmount (amount: string, symbol: string, decimals: number): string {
     return amount.unShiftedBy_(decimals).remainDecimal_(decimals) + ' ' + symbol
   }
 
-  decodeAmount (str) {
+  decodeAmount (str: string): {amount: string, symbol: string, decimals: number} {
     const [amountWithDecimals, symbol] = str.split(' ')
     const [_, decimalsZero] = amountWithDecimals.split('.')
     return {
